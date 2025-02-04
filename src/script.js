2473d00303406f3102801dcde5c4ddc42dc0c0d3
Marcel <izuobasomtochukwu@gmail.com>
2025-02-04 22:20:21 +0100

    media queries

--- src/script.js
+++ src/script.js
@@ -1,12 +1,7 @@
 const menuToggle = document.getElementById("menuToggle");
 const menu = document.getElementById("menu");
-const overlay = document.querySelector('.overlay')
 
 menuToggle.addEventListener("click", () => {
     menu.classList.toggle("active");
-    menuToggle.classList.contains("open") ? "block" : "none";
-});
-overlay.addEventListener("click ",() => {
-    menu.classList.remove("open");
-    overlay.style.display = "none";
+    menuToggle.classList.toggle("open");
 });
