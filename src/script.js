f252d0f5b0ad586e6bc41d341f995743115c2375
Marcel <izuobasomtochukwu@gmail.com>
2025-02-04 21:26:36 +0100

    home-page query

--- src/script.js
+++ src/script.js
@@ -1,7 +1,12 @@
 const menuToggle = document.getElementById("menuToggle");
 const menu = document.getElementById("menu");
+const overlay = document.querySelector('.overlay')
 
 menuToggle.addEventListener("click", () => {
     menu.classList.toggle("active");
-    menuToggle.classList.toggle("open");
+    menuToggle.classList.contains("open") ? "block" : "none";
+});
+overlay.addEventListener("click ",() => {
+    menu.classList.remove("open");
+    overlay.style.display = "none";
 });
