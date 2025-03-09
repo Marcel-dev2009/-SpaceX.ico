ed28c32e67e45a3d7c8abe933c5dae84d99bf15c
Marcel <izuobasomtochukwu@gmail.com>
2025-03-09 12:42:44 +0100

    updated js and styles

--- src/script.js
+++ src/script.js
@@ -1,9 +1,17 @@
 const menuToggle = document.getElementById("menuToggle");
 const menu = document.getElementById("menu");
+const blurEFFECT = document.querySelector(".content");
+const blur = document.querySelector(".container");
+/* const counter = document.querySelector(".counter");
+const menuList = document.querySelector(".menu"); */
 
 menuToggle.addEventListener("click", () => {
     menu.classList.toggle("active");
     menuToggle.classList.toggle("open");
+    blurEFFECT.classList.toggle("menu-open");
+    blur.classList.toggle("menu-open");
+   /*  counter.classList.toggle("opened");
+    menuList.classList.toggle("opened"); */
 });
 
 /* const animation = lottie.loadAnimation({
