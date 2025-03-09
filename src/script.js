f6c0878c8f2f47bc989eb15b81e6bc9ba415abf1
Marcel <izuobasomtochukwu@gmail.com>
2025-03-09 04:37:54 +0100

    updated designs

--- src/script.js
+++ src/script.js
@@ -5,3 +5,13 @@ menuToggle.addEventListener("click", () => {
     menu.classList.toggle("active");
     menuToggle.classList.toggle("open");
 });
+
+/* const animation = lottie.loadAnimation({
+    container: document.getElementById('uni-animate'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'Rocket_custom_icon (1).json'
+}
+
+) */
